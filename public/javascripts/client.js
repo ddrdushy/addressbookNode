0aa3f19abdd7ac46af53cc13c90d546ca19b49e0
ddrdushy <dushy2009@gmail.com>
2017-05-10 17:33:17 +0530

    data is loaded to the table

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -4,5 +4,23 @@ $(document).ready(function () {
 });
 
 function loadData(){
+    $.ajax({
+        type: "GET",
+        url: "/api/all/"
+    })
+        .done((data) => {
+            var html = "";
+            data.map((e)=>{
+                console.log(e);
+                html+=`<tr><td><a href="#" onclick="datoToForm(${e.id})">${e.name}</a></td></tr>`
+            });
+            $('#loadData').html(html);
+        })
+        .fail((err)=>{
+
+    });
+};
+
+function datoToForm(id){
 
 };
