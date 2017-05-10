5a5d0d86f0bddfedecef0fe2d56090e541b7bf61
ddrdushy <dushy2009@gmail.com>
2017-05-10 13:45:44 +0530

    api created

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var data = require('../src/api/contacts');
+
 router.get('/all', function(req, res, next) {
-    res.send('respond with a resource');
+    data.all()
+        .then((data)=>{
+            res.json(data);
+        });
 });
 
 router.post('/add', function(req, res, next) {
--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,7 +1,15 @@
+const knex = require('../utils/knex');
 var contacts = {};
 
-contacts.all = ()=>{
-
+contacts.all = () => {
+    return knex()
+        .select()
+        .from('data')
+        .then((data,err)=>{
+            if(err)
+                return;
+            return data;
+        });
 };
 
 module.exports = contacts;
